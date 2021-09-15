f3e49c344c6bd4afe25334a229539101fdd21afb
Konstantin T <tconsta@users.noreply.github.com>
2021-09-15 15:10:13 +0700

    added image processing on the fly

--- app/pixel.py
+++ app/pixel.py
@@ -1,12 +1,24 @@
 from PIL import Image
+from io import BytesIO
+from base64 import b64encode
 
 
-def process_image():
-
+def process_image(img_b, color_hex=None):
+    """Counts black, white, and specified color pixels and mask them on the image.
+    Args:
+        img_b (bytes): image to process
+        hex_color (str): color in hex format
+    Returns:
+        bytes: processed image with masked pixels,
+        dict: {'blacks': str,
+               'whites': str,
+               'colored': str}
+    """
     black_rgb = (0, 0, 0)
     white_rgb = (255, 255, 255)
 
-    color_rgb = (1, 2, 3)
+    if color_hex:
+        color_rgb = tuple(int(color_hex[i:i + 2], 16) for i in (0, 2, 4))
 
     red_rgb = (255, 0, 0)       # highlight black
     blue_rgb = (0, 0, 255)      # highlight white
@@ -15,13 +27,14 @@ def process_image():
     # color counts
     blacks = whites = colored = 0
 
-    img = Image.open('/home/tconsta/pixelcount/app/test.jpg').convert("RGB")
+    img = Image.open(BytesIO(img_b)).convert("RGB")
     pixdata = img.load()
     width, height = img.size
 
+    # Count and mask
     for y in range(height):
         for x in range(width):
-            if  pixdata[x, y] == color_rgb:
+            if color_hex and pixdata[x, y] == color_rgb:
                 pixdata[x, y] = green_rgb
                 colored += 1
             elif pixdata[x, y] == black_rgb:
@@ -31,9 +44,26 @@ def process_image():
                 pixdata[x, y] = blue_rgb
                 whites += 1
 
-    img.save('result.jpg')
+    # Debug:
+    # img.save('debug.jpg')
+
+    img_io = BytesIO()
+    img.save(img_io, format='PNG')
+    # Return bytes containing the entire contents of the buffer
+    new_img_b = img_io.getvalue()
+
+    counts = {'blacks': f'{blacks} ({blacks/(width * height)*100:.1f}%)',
+              'whites': f'{whites} ({whites/(width * height)*100:.1f}%)',
+              'colored': f'{colored} ({colored/(width * height)*100:.1f}%)'}
+
+    return new_img_b, counts
 
 
-if __name__ =='__main__':
+def img_to_datauri(img):
 
-    process_image()
+    # b64encode - Encode the bytes-like objects and return the encoded bytes
+    # bytes.decode(encoding="utf-8") - Return a string
+    data = b64encode(img).decode()
+    mime = 'image/jpeg;'
+    datauri = "data:%sbase64,%s" % (mime, data)
+    return datauri
