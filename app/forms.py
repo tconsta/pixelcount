36cf63c2ce0ec18bcb1ae8f61b761465ca7aacb3
Konstantin T <tconsta@users.noreply.github.com>
2021-09-15 15:11:47 +0700

    color form validation added

--- app/forms.py
+++ app/forms.py
@@ -1,6 +1,26 @@
 from django import forms
+from django.core.exceptions import ValidationError
+
+RGB_LENGTH = 7  # ex '#AABBFF'
 
 
 class ImgUploadForm(forms.Form):
     image = forms.ImageField(label='Select an image', required=True)
-    color = forms.CharField(label='Color in hex', required=False)
+    color = forms.CharField(label='Color in hex',
+                            required=False, max_length=RGB_LENGTH,
+                            widget=forms.TextInput(attrs={'placeholder': 'ex: F2E5A0 or #F2E5A0'}))
+
+    def clean_color(self):
+        hexcolor = self.cleaned_data['color'].strip()
+        if hexcolor == '':
+            return None
+
+        if len(hexcolor) == RGB_LENGTH and not hexcolor.startswith('#'):
+            raise ValidationError("Please enter color in the correct format or leave it blank")
+        else:
+            hexcolor = hexcolor.lstrip('#')
+        try:
+            int(hexcolor, 16)
+        except ValueError:
+            raise ValidationError("Please enter color in the correct format or leave it blank")
+        return hexcolor
